872ad489963e9bb0eea0f24ea504864a57a15d3c
tavad <a.tavadyan@gmail.com>
2023-12-18 16:49:54 +0400

    Auto commit - 2023-12-18

--- 2023_12_18_GDP_structure/GDP_structure_initial.R
+++ 2023_12_18_GDP_structure/GDP_structure_initial.R
@@ -33,14 +33,7 @@ data <-
   as_tibble()
 
 
-pal=c("#003f5c",
-      "#2f4b7c",
-      "#665191",
-      "#a05195",
-      "#d45087",
-      "#f95d6a",
-      "#ff7c43",
-      "#ffa600")
+
 
 main_data <- 
   data |> 
